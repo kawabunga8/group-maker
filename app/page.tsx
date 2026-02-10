81065c2505fae76f039ad01fe5a038a1658426e7
kawabunga8 <kawamura.shingo@gmail.com>
2026-02-10 04:12:21 +0000

    Fix hidden character in homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-⁠tsx
+tsx
 import Link from "next/link";
 
 export default function Home() {
@@ -6,10 +6,10 @@ export default function Home() {
     <main style={{ padding: 24 }}>
       <h1>Group Maker</h1>
       <p>Create a class, add students, then generate random groups.</p>
-
       <p>
-        Go to: <Link href="/class">Class page</Link>
+        Go to: <Link href="/">Home</Link>
       </p>
     </main>
   );
 }
+
